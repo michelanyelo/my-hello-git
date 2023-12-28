8ac659c1dced546d2096b8fba25dd104393ba1cc
BEBOTES <miguelmonzon.inf@gmail.com>
2023-12-28 18:45:28 -0300

    Apuntes de Git

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,11 @@
+# Apuntes
+# cd "directorio proyecto"
+# git config --global user.name "USUARIO PC"
+# git config --global user.email "USUARIO CORREO"
+# git init
+# git branch -m main (renombrar branch master a main)
+# git add "archivo.py" agregar a la cola
+# git commit -m "mensaje con los cambios hechos"
+# git checkout "archivo.py" regresar a una version anterior
+
 print("New Hello Git!")
