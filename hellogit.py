a2c192f6ed36bfdb3e2498da619612caaa854413
BEBOTES <miguelmonzon.inf@gmail.com>
2023-12-29 09:51:12 -0300

    Cambios en el apunte

--- hellogit.py
+++ hellogit.py
@@ -6,9 +6,9 @@
 # git branch -m main (renombrar branch master a main)
 # git add "archivo.py" agregar a la cola
 # git commit -m "mensaje con los cambios hechos"
-# git checkout "archivo.py" regresar a una version anterior
+# git checkout "archivo.py o hash o main" cambiar puntero a cualquier fotografía
 # git config --global alias.historial "log --graph --decorate --all --oneline" Alias historial añadido al archivo global
 # git diff Muestra los cambios de los ficheros sin tomar una fotografia
 # git reflog Muestra todo el historial de cambios
-
+# git reset --hard "hash o alias" regresar al primer cambio y tambien al ultimo
 print("New Hello Git! with changes")
